6c81546c2c6cf89ecb1d7fb2736a2d7891dc0cfe
Janjan <bubble-gumz@hotmail.com>
2017-10-14 10:55:35 +0700

    Finish

--- BasicSwift4_14oct17.playground/Contents.swift
+++ BasicSwift4_14oct17.playground/Contents.swift
@@ -3,6 +3,7 @@
 import UIKit
 
 //Variable and Constant การประกาศตัวแปร และค่าคงที่
+
 //การประกาศตัวแปรแบบแก้ไขค่าได้
 var nameString = "Doraemon"
 var numberInt = 123
@@ -11,3 +12,18 @@ var statusBool = true
 
 nameString = "Doramy"
 numberInt = 222
+
+//การประกาศตัวแปรแบบที่ไม่สามารถแก้ไขค่าได้ คือ ค่าคงที่นั่นเอง
+let surnameString = "MySurname"
+//surnameString = "ChangeSurname"
+
+//การกำหนด Data Type
+var studentString: String = "Nobita"
+var numInt: Int = 333
+
+//Display on Terminal
+print("Display on Terminal")
+print("Name ==> \(nameString)")
+print("number ==> \(numberInt + 100)")
+
+
