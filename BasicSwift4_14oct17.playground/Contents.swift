e42429f82603e4d06c4a2076c764a2a6db1aade1
Janjan <bubble-gumz@hotmail.com>
2017-10-14 10:39:21 +0700

    Variable type var

--- BasicSwift4_14oct17.playground/Contents.swift
+++ BasicSwift4_14oct17.playground/Contents.swift
@@ -2,4 +2,12 @@
 
 import UIKit
 
-var str = "Hello, Master"
+//Variable and Constant การประกาศตัวแปร และค่าคงที่
+//การประกาศตัวแปรแบบแก้ไขค่าได้
+var nameString = "Doraemon"
+var numberInt = 123
+var numberDouble = 12.345
+var statusBool = true
+
+nameString = "Doramy"
+numberInt = 222
