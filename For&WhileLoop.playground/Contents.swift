e466b39324258ce3a20e7cd9cf327ae04a6bd30d
Janjan <bubble-gumz@hotmail.com>
2017-10-15 11:53:35 +0700

    While Loop OK

--- For&WhileLoop.playground/Contents.swift
+++ For&WhileLoop.playground/Contents.swift
@@ -2,13 +2,40 @@
 
 import UIKit
 
-//Sample How to Use For Loop
+
+//Sample How to use For Loop
 let nameString = ["Doraemon", "Doraeme", "Nobita", "Janjan"]
+
+let numberInt = [1,2,3,4,5]
+
 var numInt = 0
 
+//Type String
 for strFriend in nameString {
     print("Name of friend ==> \(strFriend)")
     numInt += 1
     print("คนที่ \(numInt)")
 }
+
+//Type Integer
+for positionArray in numberInt {
+    print("Array[\(positionArray)] ==> \(numberInt[positionArray-1])")
+}
+
+
+//Sample How to use While Loop
+var factorInt = 1 // lastest factoryInt ==>12
+while factorInt <= 12 {
+    print("รอบที่ \(factorInt)")
+    factorInt += 1
+}
+
+
+
+
+
+
+
+
+
 
